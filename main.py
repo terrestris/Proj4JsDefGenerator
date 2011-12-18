c24eaabf36c64d9c0fce0e3cda0ad27c04c16c33
chrismayer <mayer@terrestris.de>
2011-12-18 16:02:24 +0100

    added comments to source code of main.py, no functional change

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ __license__= 'MIT License (see ./license.txt)'
 import sys
 
 def print_logo_screen( ):
-    """Prints debugging information when the script encounters an illegal color."""
+    """Prints a software logo to the standard out"""
 
     print ""
     print " _____           _ _  _       _     _____        __          "
@@ -37,18 +37,24 @@ def print_logo_screen( ):
     print ""
 
 
-
 try:
 
     print_logo_screen()
-
+    
+    # check if an argument has been passed
     if len(sys.argv) == 2:
+        
+        # read out epsg code from argument
         epsg = sys.argv[1]
         found = False
 
         try:
+            # cast to int which implicitly checks
+            # if passed value is an integer number
             iEpsg = int(epsg)
             
+            # use current epsg file of Ubuntu if available
+            # otherwise use the local copy
             try:
                f = open("/usr/share/proj/epsg", "r")
             except IOError as e:
@@ -57,6 +63,8 @@ try:
             projTag = "<" + epsg + ">"
             jsDef = ""
 
+            # search for correct epsg definition and
+            # transform it to Proj4Js structure
             for line in f:
                 if line.startswith(projTag) == True:
                     cleanedLine = line.replace(projTag, "");
@@ -64,24 +72,31 @@ try:
                     cleanedLine = cleanedLine.replace("\n", "");
                     jsDef = "Proj4js.defs[\"EPSG:" + epsg + "\"] = \"" + cleanedLine + "\";"
 
+                    # print out epsg definition
                     print "Copy this ..."
                     print " " 
                     print jsDef
 
+                    # create a Proj4Js definition file with the epsg definition
                     w = open("./defs/EPSG" + epsg + ".js", "w+")
                     w.write(jsDef)
                     w.close()
+                    
+                    # print out path to Proj4Js definition file
                     print " " 
                     print "... or look at ./defs/EPSG" + epsg + ".js"
                     print " "
                     found = True
 
+            # print info if no matching epsg code has been found
             if found == False:
                 print "Provided EPSG code was not found in the list ..."
 
+        # info if no integer number has been passed as epsg argument
         except ValueError:
            print "Please provide a valid EPSG with only the numeric part, e.g. 4326 in order to get the Proj4JS definition of EPSG:4326 ..."
-
+   
+    # info in case of missing epsg argument
     else:
         print "Please provide a valid EPSG code as argument..."
 
